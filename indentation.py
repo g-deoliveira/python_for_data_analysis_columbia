c8487b21f50a468a50dd4b01094e40d8191d120f
Guilherme <gdeoliveira@readyresponders.com>
2022-01-31 22:32:41 -0500

    remove empty lines

--- indentation.py
+++ indentation.py
@@ -12,37 +12,21 @@ roof_rack_for_bike = True
 print('You are driving a ', vehicle)
 
 if door_count == 4:
-
     print('Your vehicle has', door_count, 'doors.')
-
 else:
-
     print('Your vehicle does not have 4 doors.')
-
 if sunroof:
-
     print('Your vehicle has a sunroof.')
-
 else:
-
     print('Your vehicle does not have a sunroof.')
 
 if roof_rack:
-
     print('Your vehicle has a roof rack.')
-
     if roof_rack_for_skis:
-
         print('The roof rack can transport skis.')
-
     elif roof_rack_for_bike:
-
         print('The roof rack can transport bikes.')
-
     else:
-
         print('The roof rack is a standard roof rack.')
-
 else:
-
     print('The vehicle does not have a roof rack.')
