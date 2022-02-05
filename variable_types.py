5658294aeba604d59c5e6d4f90cccf3d8b0928ab
Guilherme <gdeoliveira@readyresponders.com>
2022-02-05 15:26:47 -0500

    add type checks

--- variable_types.py
+++ variable_types.py
@@ -3,16 +3,20 @@
 
 i = 80   # this is an integer
 assert isinstance(i, int)
+assert type(i) == int
 
 x = 80.0  # this is a float
 assert isinstance(x, float)
+assert type(x) == float
 
 c = 'cat'   # this is a string
 assert isinstance(c, str)
+assert type(c) == str
 
 flag = True  # this is a boolean variable
 assert isinstance(flag, bool)
+assert type(flag) == bool
 
 y = b'oo'    # this is a byte array
 assert isinstance(y, bytes)
-
+assert type(y) == bytes
