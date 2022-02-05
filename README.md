00f940f5b8e9841388e307688d61f77ebc016fa6
Guilherme <gdeoliveira@readyresponders.com>
2022-02-05 10:07:41 -0500

    variable types and README

---
+++ README.md
@@ -0,0 +1,3 @@
+# Python for Data Analysis
+
+This repo contains a small collection of code snippets that I use for various purposes in the Python for Data Analysis course that I teach at the Program for Applied Analytics at Columbia University.
---
+++ variable_types.py
@@ -0,0 +1,18 @@
+
+
+
+i = 80   # this is an integer
+assert isinstance(i, int)
+
+x = 80.0  # this is a float
+assert isinstance(x, float)
+
+c = 'cat'   # this is a string
+assert isinstance(c, str)
+
+flag = True  # this is a boolean variable
+assert isinstance(flag, bool)
+
+y = b'oo'    # this is a byte array
+assert isinstance(y, bytes)
+
