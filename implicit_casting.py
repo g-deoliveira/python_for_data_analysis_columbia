411c764bff6dafc605f999558389c72a3a277262
Guilherme <gdeoliveira@readyresponders.com>
2022-09-05 17:50:04 -0400

    updates

---
+++ implicit_casting.py
@@ -0,0 +1,20 @@
+# x is an int
+x = 1
+assert isinstance(x, int)
+
+# y is a float
+y = 99.0
+assert isinstance(y, float)
+
+# the addition returns a float
+# x is implicitly cast from an int
+# to a float during the operation
+z = x + y
+assert isinstance(z, float)
+assert z == 100.0
+assert isinstance(x, int)
+
+b = True
+# here the boolean variable b is
+# implicitly type cast to int
+assert b + 9 == 10
--- variable_types.py
+++ variable_types.py
@@ -22,21 +22,21 @@ assert isinstance(y, bytes)
 assert type(y) == bytes
 
 
-# convert numbers to strings:
+# type cast numbers to strings:
 x = 123.123
 x = str(x)
 
-# convert strings to numbers
+# type cast strings to numbers
 x = '12'
 y = '14'
 z = int(x) + int(y)
 z = str(z)
 
-# convert integers to booleans
+# type cast integers to booleans
 assert bool(1) is True
 assert bool(0) is False
 
-# this conversion will generate a ValueError
+# this type cast will generate a ValueError
 # because you can't represent 'abc' as a number
 x = 'abc'
 y = int(x)
