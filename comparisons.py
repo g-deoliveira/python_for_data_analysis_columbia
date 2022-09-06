5a348e41f7b32fe46c59e4c54ae74015594daa06
Guilherme <gdeoliveira@readyresponders.com>
2022-09-05 21:30:58 -0400

    updates

--- comparisons.py
+++ comparisons.py
@@ -17,4 +17,23 @@ assert 0 < 1 <= 1
 # are equivalent to a sequence of comparisons
 assert 0 < 1 and 1 <= 1
 
+# equality of strings
+x = "Orange"
+y = "Orange"
 
+assert x == y
+
+# comparing strings
+x = "green"
+y = "Green"
+
+assert y < x
+
+# highlighting lexicographical order
+x = "dog"
+y = "doG"
+assert y < x
+
+x = "dogs"
+y = "dog"
+assert y < x
