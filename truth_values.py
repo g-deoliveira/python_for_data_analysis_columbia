a99f9938622984e2331c097e649891e24bc23cc0
Guilherme <gdeoliveira@readyresponders.com>
2022-09-06 22:23:05 -0400

    truth_values

---
+++ truth_values.py
@@ -0,0 +1,20 @@
+
+# recall an assertion generates an assertion
+# if the condition it evaluates is False
+
+# these generate an AssertionError
+assert False
+assert None
+assert 0
+assert 0.0
+assert '' # empty string (using single quotes)
+assert "" # empty string (using double quotes)
+assert [] # empty list
+assert () # empty tuple
+
+# these all pass
+assert True
+assert 3
+assert 'cat'
+assert ' '
+assert [0]
