2ca6ff57ca7a0b8e5784845e5a21bb677401b1f6
Guilherme <gdeoliveira@readyresponders.com>
2022-09-06 22:30:51 -0400

    short_circuit_or

---
+++ short_circuit_or.py
@@ -0,0 +1,25 @@
+# suppose we want to assign `x` a
+# default value if its value is 0
+x = 0
+default = 9999
+
+y = x or default
+assert y == 9999 # this passes
+
+# suppose we want to assign `x` a
+# default value if it's an empty list
+x = []
+default = [9999]
+
+y = x or default
+assert y == [9999] # this passes
+
+
+
+# example where default is not
+# utilized
+x = 10
+default = 9999
+
+y = x or default
+assert x == 10
