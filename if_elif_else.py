e1bd9dba97fd0225f888f980e4caaa07cf32efba
Guilherme <gdeoliveira@readyresponders.com>
2022-09-11 07:49:28 -0400

    updates

--- if_elif_else.py
+++ if_elif_else.py
@@ -34,4 +34,13 @@ else:
     else:
         print('y is zero')
 
+temperature = 33
+precipitation_probability = 45
+snow_probability = 45
+wind_speed = 45
+wind_gust = 62
 
+if temperature < 40 and precipitation_probability > 66 and snow_probability > 66 and wind_speed > 45 and wind_gust > 60:
+    cancel_race = True
+else:
+    cancel_race = False
