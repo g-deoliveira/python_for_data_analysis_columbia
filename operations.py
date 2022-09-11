e013d650757dc18c18ce490ebb1800be37c95dd8
Guilherme <gdeoliveira@readyresponders.com>
2022-09-11 14:25:26 -0400

    updates

--- operations.py
+++ operations.py
@@ -1,26 +1,28 @@
-x = 10
-y = 3
+# power operator for exponentiation
+assert 100 == 10 ** 2
+assert 10 == 100 ** 0.5 # fractional exponents
+assert 0.1 == 10 ** -1 # negative exponents
 
-# exponentiation
-assert 1000 == x ** y
+# note you can use the built-in operator `pow`
+assert pow(2, 3) == 8
 
 # multiplication
-assert 30 == x * y
+assert 30 == 10 * 3
 
 # division
-assert 3 + 1 / 3 == x / y
+assert 2.25 == 9 / 4
 
 # note, division returns a float
 assert 10 / 2 == 5.0
 
-# floot division
-assert 3 == x // y
+# floor division
+assert 2 == 9 // 4 # returns an int
 
 # addition
-assert x + y == 13
+assert 10 + 3 == 13
 
 # subtraction
-assert x - y == 7
+assert 10 - 3 == 7
 
 # modulus
-assert x % y == 1
+assert 11 % 4 == 3
