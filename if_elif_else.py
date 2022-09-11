c2b9acbf30c7098dd8bafb34b56905dc2e1b88ce
Guilherme <gdeoliveira@readyresponders.com>
2022-09-10 22:42:44 -0400

    updates

--- if_elif_else.py
+++ if_elif_else.py
@@ -1,11 +1,19 @@
 x = 10
 
-if x < 0:
+if x is None:
+    print('x is None')
+elif x < 0:
     print('x is negative')
 elif x > 0:
     print('x is positive')
 else:
     print('x is zero')
 
+if x < 0:
+    print('x is negative')
+elif x > 0:
+    print('x is positive')
+else:
+    print('x is zero')
 
 
