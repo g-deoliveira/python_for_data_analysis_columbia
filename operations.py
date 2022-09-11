4016c8f9b841f107901887fdf827f9091ff032a8
Guilherme <gdeoliveira@readyresponders.com>
2022-09-11 14:17:58 -0400

    add operations.py

---
+++ operations.py
@@ -0,0 +1,26 @@
+x = 10
+y = 3
+
+# exponentiation
+assert 1000 == x ** y
+
+# multiplication
+assert 30 == x * y
+
+# division
+assert 3 + 1 / 3 == x / y
+
+# note, division returns a float
+assert 10 / 2 == 5.0
+
+# floot division
+assert 3 == x // y
+
+# addition
+assert x + y == 13
+
+# subtraction
+assert x - y == 7
+
+# modulus
+assert x % y == 1
