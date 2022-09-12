acbaee8b8eb1767a689c81f568cc49fde9facec5
Guilherme <gdeoliveira@readyresponders.com>
2022-09-11 20:07:34 -0400

    updates

---
+++ functions_motivation_4.py
@@ -0,0 +1,6 @@
+from input_functions import clean_up, get_value
+
+value_1 = clean_up(get_value(1))
+value_2 = clean_up(get_value(2))
+
+print(f"\nThe cleaned up values are {value_1} and {value_2}.\n")
