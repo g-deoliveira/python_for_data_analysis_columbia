b45cc30153e7c8930aa38ff771e68524bef66602
Guilherme <gdeoliveira@readyresponders.com>
2022-09-12 21:33:59 -0400

    updates

--- value_comparisons.py
+++ value_comparisons.py
@@ -1,13 +1,15 @@
-# define these variables
+# define some variables
 temperature = 82
 is_raining = True
 
+# assign the results of comparisons to other variables
 is_hot_day = temperature > 80
 is_hot_and_raining = is_hot_day and is_raining
 
 assert is_hot_day
 assert is_hot_and_raining
 
+# define some variables
 x = 10
 y = "cat"
 z = -4
@@ -20,7 +22,10 @@ assert test_1
 test_2 = (x >= 10) and (y == "cat") and (z < 0)
 assert test_1 == test_2
 
-test_3 = x > 20 or y == "bat" or z < 0
+# you can also group together comparisons
+# in parentheses
+test_3 = ((x >= 10 and y == "cat" and z < 0)
+            or (x <=10 and y == "bird"))
 assert test_3
 
 
