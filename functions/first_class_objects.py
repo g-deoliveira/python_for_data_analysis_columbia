d25df41c745c71b68aa392845e9d8b1a3d6c6e3a
Guilherme <gdeoliveira@readyresponders.com>
2023-02-02 16:49:22 -0500

    first_class_objects

---
+++ functions/first_class_objects.py
@@ -0,0 +1,32 @@
+def my_sum(a, b):
+    return a + b
+
+add = my_sum
+assert add(1,2) == 3
+
+def my_operation(a, b, func):
+    return func(a,b)
+
+assert my_operation(1, 2, my_sum) == 3
+
+def return_x(x):
+    return x
+
+def get_max_min(what):
+
+    if what == "min":
+        return min
+    elif what == "max":
+        return max
+    else:
+        return return_x
+
+
+f = get_max_min("max")
+assert f([1,2,3]) == 3
+
+f = get_max_min("min")
+assert f([1,2,3]) == 1
+
+f = get_max_min("")
+assert f([1,2,3]) == [1,2,3]
