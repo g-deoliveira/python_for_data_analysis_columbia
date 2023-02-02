61b26023b5887a9ce3494e3b930e1383f3d11edb
Guilherme <gdeoliveira@readyresponders.com>
2023-02-01 23:39:23 -0500

    combined

--- functions/input_args.py
+++ functions/input_args.py
@@ -1,14 +1,20 @@
 def positional_args(x, y, z):
-    return x * y + z
+    return x / y + z
 
-assert positional_args(2, 4, 8) == 16
-assert positional_args(4, 2, 8) == 16
-assert positional_args(4, 8, 2) == 34
+assert positional_args(4, 2, 5) == 7
+assert positional_args(2, 4, 5) == 5.5
+assert positional_args(4, 5, 2) == 2.8
 
 
 def keyword_args(x, y, z):
-    return x * y + z
+    return x / y + z
 
-assert keyword_args(x=2, y=4, z=8) == 16
-assert keyword_args(y=4, x=2, z=8) == 16
-assert keyword_args(y=4, z=8, x=2) == 16
+assert keyword_args(x=4, y=2, z=5) == 7
+assert keyword_args(y=2, x=4, z=5) == 7
+assert keyword_args(y=2, z=5, x=4) == 7
+
+
+assert keyword_args(2, 4, z=5) == 5.5
+assert keyword_args(2, y=4, z=5) == 5.5
+assert keyword_args(2, z=5, y=4) == 5.5
+
