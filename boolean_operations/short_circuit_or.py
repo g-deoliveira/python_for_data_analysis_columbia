d0b592387acfca30d0d21890f878d1ed8164f206
Guilherme <gdeoliveira@readyresponders.com>
2023-02-03 00:39:30 -0500

    short circuiting

--- boolean_operations/short_circuit_or.py
+++ boolean_operations/short_circuit_or.py
@@ -1,43 +1,30 @@
-# val is equal to x < y
-# and y < z is not evaluated
 x,y,z = 0, 5, 10
 val = x < y or y < z
 assert val
 
-# val is equal to y < z
 x,y,z = 10,5,3
 val = x < y or y < z
 assert not val
 
-# val takes on the value of the
-# first truth that is encountered
 x,y,z = 100, 5, 10
 val = x or y or z
 assert val == x
 
-# val takes on the value of the
-# first truth that is encountered
 x,y,z = 0, 5, 10
 val = x or y or z
 assert val == y
 
-# val takes on the value of the
-# first truth that is encountered
 x,y,z = 0, 0, 10
 val = x or y or z
 assert val == z
 
-# here val takes on the value of the
-# last value
 x,y,z = 0, 0, ""
 val = x or y or z
 assert val == z
 
-# suppose we want to assign `x` a
-# default value if its value is 0
-# (or None, empty list, ...)
-x = 0
-default = 9999
+def black_box():
+    return None
 
-y = x or default
+default = 9999
+y = black_box() or default
 assert y == 9999
