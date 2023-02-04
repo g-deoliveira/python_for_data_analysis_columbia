ee6452854f2b5f17b3fe7d308d6aaf2e56f24ac1
Guilherme <gdeoliveira@readyresponders.com>
2023-02-04 14:06:18 -0500

    short circuit

--- boolean_operations/short_circuit_and.py
+++ boolean_operations/short_circuit_and.py
@@ -16,7 +16,10 @@ x,y,z = 0, 0, 10
 val = x and y and z
 assert val == y
 
-x,y,z = 0, 0, [] # empty list
-val = x or y or z
-assert val == z
-
+x,y = 0, "1"
+val = False
+if (isinstance(x, int)
+    and isinstance(y, int)
+    and x < y):
+    val = True
+assert not val
