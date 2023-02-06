aa5bef5decd916bd28871649e737b257de0db457
Guilherme <gdeoliveira@readyresponders.com>
2023-02-06 16:22:38 -0500

    infinite loops

--- while/while.py
+++ while/while.py
@@ -23,3 +23,19 @@ while True:
     x = x + 1
     if x > 1:
         break
+
+
+# infinite loops, hit Ctrl-C!!
+
+x = 0
+while True:
+    if x % 1 == 0:
+        x = x + 1
+    else:
+        x = x - 1
+
+
+x = 0
+counter = 0
+while x > 0:
+    counter = counter + 1
