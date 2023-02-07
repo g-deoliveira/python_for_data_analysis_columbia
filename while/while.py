6d97cfaf9bf591d83c2eccfc21e588044da5908c
Guilherme <gdeoliveira@readyresponders.com>
2023-02-06 20:07:59 -0500

    augmented op

--- while/while.py
+++ while/while.py
@@ -6,13 +6,13 @@ while x > 1:
 x = 0
 while x < 3:
     print(f"in while loop 2 with x={x}")
-    x = x + 1
+    x += 1
 
 
 x = 0
 while x < 3:
     print(f"in while loop 3 with x={x}")
-    x = x + 1
+    x += 1
     if x > 1:
         break
 
@@ -20,7 +20,7 @@ while x < 3:
 x = 0
 while True:
     print(f"in while loop 3 with x={x}")
-    x = x + 1
+    x += 1
     if x > 1:
         break
 
@@ -30,12 +30,12 @@ while True:
 x = 0
 while True:
     if x % 1 == 0:
-        x = x + 1
+        x += 1
     else:
-        x = x - 1
+        x -= 1
 
 
 x = 0
 counter = 0
 while x > 0:
-    counter = counter + 1
+    counter += 1
