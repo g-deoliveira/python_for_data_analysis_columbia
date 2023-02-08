77ea5cb45a82c37400212d635a69098f498e2a80
Guilherme <gdeoliveira@readyresponders.com>
2023-02-07 23:31:32 -0500

    sum first n digits

--- for_loops/sum_first_n_digits.py
+++ for_loops/sum_first_n_digits.py
@@ -1,38 +1,13 @@
-s = 0
-for k in [1,2,3]:
-    s += k
-
-assert s == 3 * 4 / 2
-
-
-
-s = 0
-for k in [1,2,3,4]:
-    s += k
-
-assert s == 4 * 5 / 2
-
-s = 0
-for k in [1,2,3,4,5]:
-    s += k
-
-assert s == 5 * 6 / 2
-
-s = 0
-for k in range(5):
-    s += k
-
-assert s == 4 * 5 / 2
-
 def sum_n_digits(n):
     s = 0
     for k in range(n):
         s += k
     return s
 
-for k in range(100):
-    if k % 10 != 0:
-        continue
+for k in range(101):
     sum_n = sum_n_digits(k+1)
+    exact = k * (k + 1) / 2
+    assert sum_n == exact
 
-    print(sum_n, k * (k+1)/2)
+    if k % 10 == 0:
+        print(f"{k:3d}: {sum_n:6,d}")
