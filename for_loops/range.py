20da4609c943a2af48e910c54e5c14957479f619
Guilherme <gdeoliveira@readyresponders.com>
2023-02-07 23:10:29 -0500

    for loops

---
+++ for_loops/range.py
@@ -0,0 +1,13 @@
+
+for k in range(5):
+    print(k)
+
+for k in range(601):
+    if k % 200 == 0:
+        print(k)
+
+for k in range(0, 601, 200):
+    print(k)
+
+for k in range(3, 0, -1):
+    print(k)
