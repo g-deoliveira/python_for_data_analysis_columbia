35fe524be4cb2ae5148ec476a51bfbe4e6a095e5
Guilherme <gdeoliveira@readyresponders.com>
2023-02-08 21:28:11 -0500

    first commit

---
+++ lists/len.py
@@ -0,0 +1,11 @@
+x = []
+assert len(x) == 0
+
+x = ["hello world"]
+assert len(x) == 1
+
+x = [True, False, ]
+assert len(x) == 2
+
+x = list(range(10))
+assert len(x) == 10
---
+++ lists/membership.py
@@ -0,0 +1,6 @@
+x = [4, 4, "x", "yz"]
+
+assert 4 in x
+assert "x" in x
+
+assert 3 not in x
