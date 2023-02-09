f756fb0e6e0ab985d11617f2d1ca4f8bc68532ff
Guilherme <gdeoliveira@readyresponders.com>
2023-02-08 22:15:28 -0500

    first commit

---
+++ lists/nested_lists.py
@@ -0,0 +1,12 @@
+x = ["a", "b"]
+
+x[1] = ["b", "c"]
+
+assert x == ["a", ["b", "c"]]
+assert x[0] == "a"
+assert x[1] == ["b", "c"]
+assert x[1][0] == "b"
+assert x[1][1] == "c"
+
+x[0] = [1, 2]
+assert x == [[1, 2], ["b", "c"]]
