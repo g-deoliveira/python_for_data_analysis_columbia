556b48a511416f9c86863b316efc95d50031f59d
Guilherme <gdeoliveira@readyresponders.com>
2023-02-11 21:17:38 -0500

    info

--- lists/lists.py
+++ lists/lists.py
@@ -1,9 +1,9 @@
 x = [41, 3.1415, True, "hello world"]
 
-assert x[0] == 41
-assert x[1] == 3.1415
-assert x[2] == True
-assert x[3] == "hello world"
+assert x[0] == 41 # first element
+assert x[1] == 3.1415 # second element
+assert x[2] == True # third element
+assert x[3] == "hello world" # 4th element
 
 assert x[-1] == "hello world"
 assert x[-2] == True
