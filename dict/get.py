d35cb54e99e37e5a97de2141058081b2b565a74e
Guilherme <gdeoliveira@readyresponders.com>
2023-02-15 21:17:48 -0500

    get

---
+++ dict/get.py
@@ -0,0 +1,21 @@
+
+cats = {
+    "Kingdom": "Animalia",
+    "Phylum": "Chordata",
+    "Class": "Mammalia",
+    "Order": "Carnivora",
+    "Suborder":	"Feliformia",
+    "Family": "Felidae",
+    "Subfamily": "Felinae",
+    "Genus": "Felis",
+}
+
+assert cats["Kingdom"] == "Animalia"
+assert cats.get("Kingdom") == "Animalia"
+
+# query for non-existant key
+assert cats.get("xya") == None
+
+# query for non-existant key with default
+assert cats.get("xya", 0) == 0
+
