a0153115c8685fade7e548e35390c5cd1287b003
Guilherme <gdeoliveira@readyresponders.com>
2023-02-15 20:56:07 -0500

    dict

--- dict/dict.py
+++ dict/dict.py
@@ -1,14 +1,17 @@
 my_dict = {}
 
-my_dict[5]
-my_dict["cat"]
-my_dict[(1, 2)]
-my_dict[("cat", "new york", 1)]
 
 my_dict[5] = [1,2,3,4,5,"cat", "dog"]
 my_dict["cat"] = 23
 my_dict[(1, 2)] = "a string"
 my_dict[("cat", "new york", 1)] = ("tuple", "hello", "world")
+
+
+my_dict[5]
+my_dict["cat"]
+my_dict[(1, 2)]
+my_dict[("cat", "new york", 1)]
+
 
 tina_turner = {
     "first_name": "Tina",
@@ -28,3 +31,20 @@ tina_turner = dict(
     )
 )
 
+# indexing
+assert tina_turner["first_name"] == "Tina"
+# nested dictionaries
+assert tina_turner["grammy_awards"]["won"] == 8
+
+# assigning a new key-value pair
+tina_turner["birth_year"] = 1039
+# oops made a typo, need to update
+tina_turner["birth_year"] = 1939
+
+# membership
+assert "birth_year" in tina_turner
+assert "birth_month" not in tina_turner
+
+# length
+assert len(tina_turner) == 4
+assert len(tina_turner["grammy_awards"]) == 2
