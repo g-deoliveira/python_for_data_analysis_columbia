4be8182722ca1d5752d1e62c9916f0d1087b891c
Guilherme <gdeoliveira@readyresponders.com>
2023-02-15 21:30:31 -0500

    get

--- dict/get.py
+++ dict/get.py
@@ -19,6 +19,11 @@ assert cats.get("xya") == None
 # query for non-existant key with default
 assert cats.get("xya", 0) == 0
 
-if "xya" in cats:
-    print(cats["xya"])
+# update
+D = {"x":  1, "y": 10}
+E = {"x": 20, "z":  0}
+D.update(E)
+assert D["x"] == 20
+assert D["y"] == 10
+assert D["z"] == 0
 
