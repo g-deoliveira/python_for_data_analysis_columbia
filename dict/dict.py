5b9542120f6e1f9b84d05791205b185ec4f49d74
Guilherme <gdeoliveira@readyresponders.com>
2023-02-22 16:14:56 -0500

    dict

--- dict/dict.py
+++ dict/dict.py
@@ -1,19 +1,22 @@
 my_dict = {}
 
-
 my_dict[5] = [1,2,3,4,5,"cat", "dog"]
 my_dict["cat"] = 23
 my_dict[(1, 2)] = "a string"
-my_dict[("cat", "new york", 1)] = ("tuple", "hello", "world")
+my_dict[("cat", 1)] = ("hello", "world")
+
+my_dict[5]          # the key is an integer
+my_dict["cat"]      # the key is a string
+my_dict[(1, 2)]     # the key is a tuple
+my_dict[("cat", 1)] # another tuple
+
+assert my_dict[5] == [1,2,3,4,5,"cat", "dog"]
+assert my_dict["cat"] == 23
+assert my_dict[(1, 2)] == "a string"
+assert my_dict[("cat", 1)] == ("hello", "world")
 
 
-my_dict[5]
-my_dict["cat"]
-my_dict[(1, 2)]
-my_dict[("cat", "new york", 1)]
-
-
-tina_turner = {
+singer = {
     "first_name": "Tina",
     "last_name": "Turner",
     "grammy_awards": {
@@ -22,7 +25,7 @@ tina_turner = {
     }
 }
 
-tina_turner = dict(
+singer = dict(
     first_name="Tina",
     last_name="Turner",
     grammy_awards=dict(
@@ -32,19 +35,19 @@ tina_turner = dict(
 )
 
 # indexing
-assert tina_turner["first_name"] == "Tina"
+assert singer["first_name"] == "Tina"
 # nested dictionaries
-assert tina_turner["grammy_awards"]["won"] == 8
+assert singer["grammy_awards"]["won"] == 8
 
 # assigning a new key-value pair
-tina_turner["birth_year"] = 1039
+singer["birth_year"] = 1039
 # oops made a typo, need to update
-tina_turner["birth_year"] = 1939
+singer["birth_year"] = 1939
 
 # membership
-assert "birth_year" in tina_turner
-assert "birth_month" not in tina_turner
+assert "birth_year" in singer
+assert "birth_month" not in singer
 
 # length
-assert len(tina_turner) == 4
-assert len(tina_turner["grammy_awards"]) == 2
+assert len(singer) == 4
+assert len(singer["grammy_awards"]) == 2
