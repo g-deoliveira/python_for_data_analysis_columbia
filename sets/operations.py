d563716053baa8ef39cbd66a9dac5ea314c26004
Guilherme <gdeoliveira@readyresponders.com>
2023-02-25 12:16:46 -0500

    sets

--- sets/operations.py
+++ sets/operations.py
@@ -1,24 +1,20 @@
 x = {1, 2, 3}
 y = {2, 3, 4, 5}
 
-
 z = x.intersection(y)
 assert z == {2, 3}
 assert z == {3, 2}
 assert z == x & y       # syntax
 assert x & y == y & x   # commutative
 
-
 z = x.union(y)
 assert z == {2, 3, 5, 4, 1}
 assert z == x | y      # syntax
 assert x | y == y | x  # commutative
 
-
 z = x.difference(y)
 assert z == {1}
 assert z == x - y        # syntax
-
 
 z = x.symmetric_difference(y)
 assert z == {1, 4, 5}
