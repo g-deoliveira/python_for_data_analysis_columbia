459c016c9fa80a19a6efaf9e84109cc024116a70
Guilherme <gdeoliveira@readyresponders.com>
2023-03-03 22:46:29 -0500

    copy

--- lists/copy.py
+++ lists/copy.py
@@ -2,17 +2,18 @@ x = [1, 2, 3]
 y = x
 assert id(x) == id(y)
 
-y[0] = -1
-assert x == [-1, 2, 3]
-assert y == [-1, 2, 3]
+y[0] = 999
+assert x == [999, 2, 3]
+assert y == [999, 2, 3]
 
+x = [1,2,3]
+y = list(x)
+assert id(y) != id(x)
+
+y[0] = 999
+assert x == [1, 2, 3]
+assert y == [999, 2, 3]
+
+# shallow copies
 y = x[:]
-y[0] = 10
-assert x == [-1, 2, 3]
-assert y == [10, 2, 3]
-assert id(y) != id(x)
-
 y = x.copy()
-assert x == [-1, 2, 3]
-assert y == [-1, 2, 3]
-assert id(y) != id(x)
