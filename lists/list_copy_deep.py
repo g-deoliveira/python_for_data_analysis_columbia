8a63240aaf5d9a72721cc2ebdf4ec97e86e45a24
Guilherme <gdeoliveira@readyresponders.com>
2023-03-03 23:26:43 -0500

    list copy

---
+++ lists/list_copy_deep.py
@@ -0,0 +1,17 @@
+import copy
+
+x = [["a", 1], ["b", 2], ["c", 3]]
+
+y = copy.deepcopy(x)
+
+y[0] = 999
+assert x == [["a", 1], ["b", 2], ["c", 3]]
+assert y == [999, ["b", 2], ["c", 3]]
+
+y[1][0] = "x"
+y[1][1] = 999
+assert x == [["a", 1], ["b", 2], ["c", 3]]
+assert y == [999, ["x", 999], ["c", 3]]
+
+assert id(x) != id(y)
+assert id(x[1]) != id(y[1])
