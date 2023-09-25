3ba683c65e45a96c8e4d4f15c498a9b2c6822c10
Guilherme <gdeoliveira@readyresponders.com>
2023-09-24 23:43:09 -0400

    update

--- for_loops/enumerate.py
+++ for_loops/enumerate.py
@@ -1,7 +1,9 @@
+PETS = ["cat", "bird", "dog"]
+
 counter = 0
-for pet in ["cat", "bird", "dog"]:
+for pet in PETS:
     print(counter, pet)
     counter += 1
 
-for counter, pet in enumerate(["cat", "bird", "dog"]):
+for counter, pet in enumerate(PETS):
     print(counter, pet)
