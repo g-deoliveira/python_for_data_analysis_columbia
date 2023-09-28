14a8af3a2d3f59bc0a25371e5204918a1fd6b4cd
Guilherme <gdeoliveira@readyresponders.com>
2023-09-27 23:57:24 -0400

    updates

--- functions/scope.py
+++ functions/scope.py
@@ -30,12 +30,3 @@ x = 20
 print("\nprior to change_and_return_x, x=", x)
 x = change_and_return_x(x)
 print("after the call to change_and_return_x, x=", x)
-
-
-def change_a_mutable_object(x):
-    x[0] = 0
-
-x = [10, 20, 30]
-print("\nprior to change_a_mutable_object, x=", x)
-change_a_mutable_object(x)
-print("after the call to change_a_mutable_object, x=", x)
