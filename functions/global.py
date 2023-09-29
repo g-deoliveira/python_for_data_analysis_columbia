54f269a49d1d669523438bf0f5529ac10c0c1063
Guilherme <gdeoliveira@readyresponders.com>
2023-09-28 22:39:18 -0400

    global

--- functions/global.py
+++ functions/global.py
@@ -21,6 +21,8 @@ def change_local():
     count = count + 1
     return count
 
+# assert statements
+assert count == 0
 assert the_count() == 2
 assert global_count() is None
 
@@ -32,3 +34,17 @@ except UnboundLocalError:
     print("handled UnboundLocalError")
 
 assert change_local() == 3
+
+# if we declare the variable as global within the scope of
+# the function, then we are telling the program we can
+# access it and modify it from anywhere
+
+# define a function that attempts to change the global variable
+def change_global_new():
+    global count
+    print(count)
+    count = count + 10
+    return count
+
+assert change_global_new() == 10
+assert count == 10
