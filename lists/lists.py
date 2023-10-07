fe5967605379d49287320f72e2fab3785c7f504f
Guilherme <gdeoliveira@readyresponders.com>
2023-10-06 20:22:35 -0400

    lists

--- lists/lists.py
+++ lists/lists.py
@@ -9,10 +9,3 @@ assert x[-1] == "hello world"
 assert x[-2] == True
 assert x[-3] == 3.1415
 assert x[-4] == 41
-
-x[0] = 1
-x[1] = "cat"
-x[2] = 0.1
-x[3] = False
-
-assert x == [1, "cat", 0.1, False]
---
+++ lists/mutability.py
@@ -0,0 +1,8 @@
+x = [41, 3.1415, True, "hello world"]
+
+x[0] = 1
+x[1] = "cat"
+x[2] = 0.1
+x[3] = False
+
+assert x == [1, "cat", 0.1, False]
