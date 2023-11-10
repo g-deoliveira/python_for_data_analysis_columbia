be842238281c2e67355163012f46a2877a31c7e0
Guilherme <gdeoliveira@readyresponders.com>
2023-11-10 15:10:59 -0500

    json

---
+++ file_io/to_json.py
@@ -0,0 +1,31 @@
+import json
+
+book = {
+    "title": "Clean Code",
+    "author": "Robert Martin",
+    "publication year": 2008,
+}
+
+# writing the contents of the book dictionary to a json file
+# note the contents are written on one line, which isn't
+# very easy to read
+with open("file_io/my_file_one_line.json", "w") as file:
+    json.dump(book, file)
+
+# specify a value of the indent parameter to "prettify" the output
+with open("file_io/my_file_one_line.json", "w") as file:
+    json.dump(book, file, indent=4)
+
+# read the contents of the json file:
+with open("file_io/my_file_one_line.json", "r") as file:
+    data = json.load(file)
+
+assert data == book
+
+# now convert a dictionary to a json-string notation
+book_as_string = json.dumps(book)
+assert book_as_string == '{"title": "Clean Code", "author": "Robert Martin", "publication year": 2008}'
+
+# now convert the json-string variable to a dictionary
+book_from_string = json.loads(book_as_string)
+assert book_from_string == book
