3b3015fe6d3771a656691d20214e54a3c1c32811
Guilherme <gdeoliveira@readyresponders.com>
2023-11-10 15:12:48 -0500

    json

--- file_io/to_json.py
+++ file_io/to_json.py
@@ -2,8 +2,8 @@ import json
 
 book = {
     "title": "Clean Code",
-    "author": "Robert Martin",
-    "publication year": 2008,
+    "author": "R. Martin",
+    "year": 2008,
 }
 
 # writing the contents of the book dictionary to a json file
@@ -24,7 +24,7 @@ assert data == book
 
 # now convert a dictionary to a json-string notation
 book_as_string = json.dumps(book)
-assert book_as_string == '{"title": "Clean Code", "author": "Robert Martin", "publication year": 2008}'
+assert book_as_string == '{"title": "Clean Code", "author": "R. Martin", "year": 2008}'
 
 # now convert the json-string variable to a dictionary
 book_from_string = json.loads(book_as_string)
