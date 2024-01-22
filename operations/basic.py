fb21cc60ce23ec7db70b2dd7cf79f38288a37a83
Guilherme <gdeoliveira@readyresponders.com>
2024-01-21 21:12:08 -0500

    ops

---
+++ operations/basic.py
@@ -0,0 +1,27 @@
+# define a variable x and print to screen
+x = 1
+print(x)
+
+# update x and print to screen
+x = 2
+print(x)
+
+# define a variable y
+y = 10
+
+# assign z to the sum of x and y
+z = x + y
+print(z)
+
+# delete x
+del x
+
+# define two string variables
+cat = "cat"
+dog = "dog"
+print(cat)
+print(dog)
+
+# assign pets using string-concatenation
+pets = cat + "s" + " and " + dog + "s"
+print(pets)
