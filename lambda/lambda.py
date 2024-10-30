bc64c6ee6946b80bc84a79f10131c1f66d750502
Guilherme <gdeoliveira@readyresponders.com>
2024-10-29 21:08:30 -0400

    lambda

--- lambda/lambda.py
+++ lambda/lambda.py
@@ -3,19 +3,27 @@
 lambda x: x
 
 # it is equivalent to this function
-def example1(x):
+def example(x):
     return x
 
-# this function admits a parameter `x` and a
-# function `func` and returns `func(x)`
-def my_function(x, func):
-    return func(x)
+# example of an anonymous function:
+items = [
+    (80, 90),
+    (10, 20),
+]
+items.sort(key=lambda x: x[1])
+assert items == [(10, 20), (80, 90)]
 
-# here we provide a default value for `func` so that
-# my_function(x) just returns x if `func` is not
-# specified
-def my_function(x, func=lambda x: x):
-    return func(x)
+# non-anonymous formulation:
+def second_element(x):
+    return x[1]
+items.sort(key=second_element, reverse=True)
+assert items == [(80, 90), (10, 20)]
 
-assert my_function("cat") == "cat"
-assert my_function("cat", lambda x: x[-1]) == "t"
+# multi-variable lambda function:
+f = lambda x,y: x + y
+assert f(1, 2) == 3
+
+# using conditional operator
+f = lambda x: "even" if x % 2 == 0 else "odd"
+assert f(2) == "even"
