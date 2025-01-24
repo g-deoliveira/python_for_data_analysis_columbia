4ccc8ec6e01ea7a84a130c890cc591ebcf552271
Guilherme de Oliveira <deoliveira.g@gmail.com>
2025-01-24 13:38:12 -0500

    add black

---
+++ black_pre_format.py
@@ -0,0 +1,14 @@
+x=1
+y  =   2 
+
+    # this is a comment
+if x== y :
+    print('Yay!!! x==y')
+else:
+    print('Oh no, x!=y')
+
+
+def favorite_sport(sport = 'tennis'):
+    return "My favorite sport is " + sport + '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
+
+favorite_sport('skiing')
