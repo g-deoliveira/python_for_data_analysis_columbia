ad3783960edc1145a8dea3abb03e4c52af10fb0a
Guilherme de Oliveira <deoliveira.g@gmail.com>
2025-01-26 21:29:31 -0500

    composition

--- functions/composition.py
+++ functions/composition.py
@@ -1,9 +1,17 @@
 
 def my_sum(a, b):
-
     return a + b
-
 
 assert my_sum(1, 2) == 3
 
-assert my_sum(my_sum(1,2), my_sum(3, 4)) == 10
+assert (
+    my_sum(
+        my_sum(1, 2), -3
+    ) == 0
+)
+
+assert (
+    my_sum(
+        my_sum(1, 2), my_sum(3, 4)
+    ) == 10
+)
